ff12fe30584d348a9cac393f34ef922947173612
Kyle Naizer <kyle.naizer@gmail.com>
2015-12-29 18:00:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sandbox::Application.config.secret_key_base = '365116162cc8be94750367cd66d53207870c6772e4a234db0fc143860192384ebde38c3c30d8f04ad50358140a7917fc08cceed2cb1ea3cc0bb5408775993bde'
